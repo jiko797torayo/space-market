eccb415def45f5b586d373b28bc1ed5f92d90b7d
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-09 14:44:43 +0900

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181009053050) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",             null: false
+    t.string   "last_name",              null: false
+    t.string   "email",                  null: false
+    t.string   "icon"
+    t.string   "encrypted_password",     null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
