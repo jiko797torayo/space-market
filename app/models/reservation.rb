9e3ad9a31c688f3a2b56bd85d78663ae4687351a
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-09 15:25:54 +0900

    create reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,5 @@
 class Reservation < ApplicationRecord
-  
+  belongs_to :user
+  belongs_to :reservation_hours, dependent: :destroy
+  belongs_to :space
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009053050) do
+ActiveRecord::Schema.define(version: 20181009054315) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "day",                 null: false
+    t.integer  "reservation_user_id"
+    t.integer  "user_id"
+    t.integer  "space_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["reservation_user_id"], name: "index_reservations_on_reservation_user_id"
+    t.index ["space_id"], name: "index_reservations_on_space_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",             null: false
