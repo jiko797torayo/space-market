4b333cbd2db08fef33eee5466c15a0476cd6ca1a
manami-kk <manami.321go@gmail.com>
2018-10-11 00:33:15 +0900

    set routing and controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,4 +3,18 @@ class TestsController < ApplicationController
   def index
   end
 
+  def createone
+  end
+
+  def createtwo
+  end
+
+  def createthree
+  end
+
+  def createfour
+  end
+
+  def createfive
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'tests#index'
+  get 'tests/createone' => 'tests#createone'
+  get 'tests/createtwo' => 'tests#createtwo'
+  get 'tests/createthree' => 'tests#createthree'
+  get 'tests/createfour' => 'tests#createfour'
+  get 'tests/createfive' => 'tests#createfive'
 end
