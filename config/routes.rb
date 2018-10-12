522b76ee3ac30a030c7c57524161bec5665e9dbe
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-12 16:05:57 +0900

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   devise_for :hosts
-<<<<<<< HEAD
   devise_for :users, controllers: {
       confirmations: 'users/confirmations',
       passwords:     'users/passwords',
       registrations: 'users/registrations',
       sessions:      'users/sessions',
   }
-  root 'tests#index'
   resources :user, only: [:index, :update, :edit]
-=======
-  devise_for :users
   root 'spaces#index'
->>>>>>> master
 end
