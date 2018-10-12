6454bd4882bc75e4f24ebd964396bc677d95c155
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-12 13:31:28 +0900

    change database to mysql

--- db/migrate/20181009070347_create_spaces.rb
+++ db/migrate/20181009070347_create_spaces.rb
@@ -2,12 +2,12 @@ class CreateSpaces < ActiveRecord::Migration[5.0]
   def change
     create_table :spaces do |t|
       t.integer    :likes_count, default: 0
-      t.references :host,           index: true, foreign_key: true
-      t.references :equipment_info, index: true, foreign_key: true
-      t.references :basic_info,     index: true, foreign_key: true
-      t.references :description,    index: true, foreign_key: true
-      t.references :plan,           index: true, foreign_key: true
-      t.references :image,          index: true, foreign_key: true
+      t.references :host,           index: true
+      t.references :equipment_info, index: true
+      t.references :basic_info,     index: true
+      t.references :description,    index: true
+      t.references :plan,           index: true
+      t.references :image,          index: true
       t.timestamps
     end
   end
