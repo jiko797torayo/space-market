4942d1e3a6577bd2f0b67f81743ad3691d9b5105
manami-kk <manami.321go@gmail.com>
2018-10-12 16:13:07 +0900

    create controllers for new_space

---
+++ app/controllers/descriptions_controller.rb
@@ -0,0 +1,6 @@
+class DescriptionsController < ApplicationController
+  layout 'new_space'
+  
+  def new
+  end
+end
---
+++ app/controllers/equipments_controller.rb
@@ -0,0 +1,6 @@
+class EquipmentsController < ApplicationController
+  layout 'new_space'
+  
+  def new
+  end
+end
