6713eb62b6289118d8e3396c65d501cc51df1a4f
manami-kk <manami.321go@gmail.com>
2018-10-12 16:17:01 +0900

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'tests#index'
-  get 'tests/createone' => 'tests#createone'
-  get 'tests/createtwo' => 'tests#createtwo'
-  get 'tests/createthree' => 'tests#createthree'
-  get 'tests/createfour' => 'tests#createfour'
-  get 'tests/createfive' => 'tests#createfive'
-=======
   devise_for :hosts
   devise_for :users
   root 'spaces#index'
->>>>>>> master
+  resources :equipments
+  resources :basicinfos
+  resources :descriptions
+  resources :images
+  resources :plans
 end
