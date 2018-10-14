2fe550296269fa74041d018977a3af262e4d9793
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-14 21:45:12 +0900

    user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,37 +13,37 @@ class User < ApplicationRecord
 
   def first_name_validates
     if first_name.blank?
-      errors.add(:first_name, "名を入力してください。")
+      errors.add(:first_name, '名を入力してください。')
     end
   end
 
   def last_name_validates
     if last_name.blank?
-      errors.add(:last_name, "姓を入力してください。")
+      errors.add(:last_name, '姓を入力してください。')
     end
   end
 
   def email_validates
     if email.blank?
-      errors.add(:email, "メールアドレスを入力してください。")
+      errors.add(:email, 'メールアドレスを入力してください。')
     elsif email !~ /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-      errors.add(:email, "メールアドレスは不正な値です。")
+      errors.add(:email, 'メールアドレスは不正な値です。')
     end
   end
 
   def password_validates
     if password.blank?
-      errors.add(:password, "パスワードを入力してください。")
+      errors.add(:password, 'パスワードを入力してください。')
     elsif password.length < 8
-      errors.add(:password, "パスワードは8文字以上で入力してください。")
+      errors.add(:password, 'パスワードは8文字以上で入力してください。')
     elsif password.length > 128
-      errors.add(:password, "パスワードは128文字以内で入力してください。")
+      errors.add(:password, 'パスワードは128文字以内で入力してください。')
     end
   end
 
   def password_confirmation_validates
     if password_confirmation != password
-      errors.add(:password_confirmation, "パスワード（確認）とパスワードの入力が一致しません。")
+      errors.add(:password_confirmation, 'パスワード（確認）とパスワードの入力が一致しません。')
     end
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(8)
+    first_name            { Faker::Name.first_name }
+    last_name             { Faker::Name.last_name }
+    email                 { Faker::Internet.free_email }
+    icon                  { File.open("#{Rails.root}/public/images/notrobot.png") }
+    password              { password }
+    password_confirmation { password }
+  end
+end
