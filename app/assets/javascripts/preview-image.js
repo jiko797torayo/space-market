51ee09c41579086acee425bb48cb8655bdcb0a94
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-14 17:18:40 +0900

    image preview version3

--- app/assets/javascripts/preview-image.js
+++ app/assets/javascripts/preview-image.js
@@ -2,6 +2,7 @@ $(document).on("turbolinks:load", function() {
   $('#user_icon').on('change', function (e) {
     var reader = new FileReader();
     reader.onload = function (e) {
+      $("#preview").empty();
       $("#preview").attr('src', e.target.result);
     };
     reader.readAsDataURL(e.target.files[0]);
