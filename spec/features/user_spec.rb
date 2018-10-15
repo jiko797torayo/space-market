baabe641154b754daa3e6380209addb9f85a69cc
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-15 13:22:52 +0900

    user join test

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'user', type: :feature do
+  let(:user) { create(:user) }
+  scenario 'sign up' do
+
+    visit root_path
+    expect(page).to have_no_content('ログアウト')
+
+    visit new_user_registration_path
+    fill_in 'user_first_name', with: user.first_name
+    fill_in 'user_last_name', with: user.last_name
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    fill_in 'user_password_confirmation', with: user.password_confirmation
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('ログアウト')
+  end
+end
