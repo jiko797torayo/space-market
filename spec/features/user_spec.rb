8640ede57ed8e11c8ebbea23ddf989faaa754778
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-15 19:14:07 +0900

    add sign in test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'user', type: :feature do
   let(:user) { create(:user) }
   scenario 'sign up' do
-
     visit root_path
     expect(page).to have_no_content('ログアウト')
 
@@ -14,6 +13,18 @@ feature 'user', type: :feature do
     fill_in 'user_password', with: user.password
     fill_in 'user_password_confirmation', with: user.password_confirmation
     check   'user_accepted'
+    find('input[name="commit"]').click
+    expect(page).to have_content('ログアウト')
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'sign in' do
+    visit root_path
+    expect(page).to have_no_content('ログアウト')
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
     find('input[name="commit"]').click
     expect(page).to have_content('ログアウト')
     expect(current_path).to eq root_path
