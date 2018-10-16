c766b534241b12e472822b099aff0d7bbe83edf1
manami-kk <manami.321go@gmail.com>
2018-10-16 15:49:58 +0900

    edit description model

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,3 +1,7 @@
 class Description < ApplicationRecord
   belongs_to :space
+
+  validates :catch_copy,       presence: true, length: { maximum: 64 }
+  validates :overview,         presence: true, length: { in: 10..800 }
+  validates :about_facilities, presence: true, length: { in: 10..800 }
 end
