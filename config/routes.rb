bb8927e374b54bff0eb518b38cc8aa0fc2b628d1
manami-kk <manami.321go@gmail.com>
2018-10-16 15:46:06 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :hosts
   devise_for :users
   root 'spaces#index'
-  resources :equipments
-  resources :basicinfos
+  resources :spaces
+  resources :equipment_infos
+  resources :basic_infos
   resources :descriptions
   resources :images
   resources :plans
