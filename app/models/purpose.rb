06e2f1db548a9ba17a4ab4639b09d3ac6368c314
manami-kk <manami.321go@gmail.com>
2018-10-16 15:49:25 +0900

    edit purpose model

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,3 +1,16 @@
 class Purpose < ApplicationRecord
   belongs_to :basic_info
+
+  validates :party, precense: true
+  validates :meeting, precense: true
+  validates :photo_shoot, precense: true
+  validates :film_shoot, precense: true
+  validates :event, precense: true
+  validates :performance, precense: true
+  validates :studio, precense: true   
+  validates :sports, precense: true
+  validates :office, precense: true
+  validates :wedding, precense: true
+  validates :other, precense: true
+  validates :basic_info, precense: true, uniqueness: true
 end
