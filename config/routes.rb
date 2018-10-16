442354adc8eb34a09718b7b3d55c27ffbe27ae05
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-16 14:18:15 +0900

    delete unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :hosts
-  devise_for :users, controllers: {
-      confirmations: 'users/confirmations',
-      passwords:     'users/passwords',
-      registrations: 'users/registrations',
-      sessions:      'users/sessions',
-  }
+  # devise_for :hosts
+  devise_for :users, only:[:all],
+                               controllers: {
+                                  registrations: 'users/registrations',
+                                  sessions:      'users/sessions'
+                                }
   root 'spaces#index'
-  devise_scope :user do
-    get 'i/login', to: 'users/sessions#new'
-    get 'i/sign_up', to: 'users/registrations#new'
+  as :user do
+    get 'i/login', to: 'users/sessions#new', as: :new_user_session
+    post 'i/login', to: 'users/sessions#create', as: :user_session
+    delete 'i/logout', to: 'users/sessions#destroy', as: :destroy_user_session
+    get 'i/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post 'i/sign_up', to: 'users/registrations#create', as: :user_registration
   end
 end
