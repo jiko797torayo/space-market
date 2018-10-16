b3845356a6b851af461ff39846cb265627a3e82d
manami-kk <manami.321go@gmail.com>
2018-10-16 15:52:09 +0900

    edit descriptions controller

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -2,5 +2,25 @@ class DescriptionsController < ApplicationController
   layout 'new_space'
   
   def new
+    @description = Description.new
+  end
+
+  def create
+    description = Description.new(description_params)
+    if description.save
+      next_page
+    else
+      render :new
+    end
+  end
+
+  private
+  def description_params
+    params.require(:description).permit(:catch_copy, :overview, :about_facilities)
+  end
+
+  def next_page
+    redirect_to root_path if params[:commit] == "保存して戻る"
+    redirect_to new_image_path if params[:commit] == "保存して進む"
   end
 end
