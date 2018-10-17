87b6815ade7befbf6c1e560249ec4e0e4d039187
manami-kk <manami.321go@gmail.com>
2018-10-17 18:15:06 +0900

    edit plan create function

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,41 @@
 class PlansController < ApplicationController
   layout 'new_space'
-  
+
   def new
+    @@space = Space.find_by(id: params[:space_id])
+    @plan = Plan.new
+    @plan.build_rental_day
+    @plan.build_rental_hour
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.space = @@space
+    if @plan.save
+      if 0 <= @plan.rental_hour[:start_hour] && @plan.rental_hour[:end_hour] < 0
+        @plan.rental_hour[:zero] == 1 
+        @plan.save
+      elsif 1 <= @plan.rental_hour[:start_hour] && @plan.rental_hour[:end_hour] < 2
+        @plan.rental_hour[:one] == 1 
+        @plan.save
+      end
+      next_page
+    else
+      render :new
+    end
+  end
+
+  private
+  def plan_params
+    params.require(:plan).permit(
+      :name, :about_plan, :price_per_hour, :price_per_day, :reservation_approval_method,
+      rental_day_attributes: [:id, :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday],
+      rental_hour_attributes: [:start_hour, :end_hour]
+    )
+  end
+
+  def next_page
+    redirect_to root_path if params[:commit] == "保存して戻る"
+    redirect_to root_path if params[:commit] == "保存して進む"
   end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,15 @@ class Plan < ApplicationRecord
   belongs_to :space
   has_one :rental_day, dependent: :destroy
   has_one :rental_hour, dependent: :destroy
+
+  accepts_nested_attributes_for :rental_day
+  accepts_nested_attributes_for :rental_hour
+
+  validates :name, presence: true
+  validates :price_per_day, presence: true
+  validates :reservation_approval_method, presence: true
+
+  enum reservation_approval_method: {
+    承認なし（今すぐ予約）:1, 承認あり:2
+  }
 end
