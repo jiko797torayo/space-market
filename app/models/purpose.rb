7745865f34c5564d44e771fcd5ab975b30842dcc
manami-kk <manami.321go@gmail.com>
2018-10-17 12:29:10 +0900

    fixed purpose model

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,16 +1,15 @@
 class Purpose < ApplicationRecord
-  belongs_to :basic_info
+  belongs_to :basic_info, optional: true
 
-  validates :party, precense: true
-  validates :meeting, precense: true
-  validates :photo_shoot, precense: true
-  validates :film_shoot, precense: true
-  validates :event, precense: true
-  validates :performance, precense: true
-  validates :studio, precense: true   
-  validates :sports, precense: true
-  validates :office, precense: true
-  validates :wedding, precense: true
-  validates :other, precense: true
-  validates :basic_info, precense: true, uniqueness: true
+  validates :party, presence: true
+  validates :meeting, presence: true
+  validates :photo_shoot, presence: true
+  validates :film_shoot, presence: true
+  validates :event, presence: true
+  validates :performance, presence: true
+  validates :studio, presence: true   
+  validates :sports, presence: true
+  validates :office, presence: true
+  validates :wedding, presence: true
+  validates :other, presence: true
 end
