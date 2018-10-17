16f41a5f49d0d04bd2b6d830eced2d3a649c471f
manami-kk <manami.321go@gmail.com>
2018-10-17 12:29:49 +0900

    add space_id to descriptions controller

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -2,12 +2,14 @@ class DescriptionsController < ApplicationController
   layout 'new_space'
   
   def new
+    @@space = Space.find_by(id: params[:space_id])
     @description = Description.new
   end
 
   def create
-    description = Description.new(description_params)
-    if description.save
+    @description = Description.new(description_params)
+    @description.space = @@space
+    if @description.save
       next_page
     else
       render :new
@@ -21,6 +23,6 @@ class DescriptionsController < ApplicationController
 
   def next_page
     redirect_to root_path if params[:commit] == "保存して戻る"
-    redirect_to new_image_path if params[:commit] == "保存して進む"
+    redirect_to new_image_path(space_id: @@space.id) if params[:commit] == "保存して進む"
   end
 end
