3c79c1ce9d221ea01cef80c126ea0c9da24fe2b8
manami-kk <manami.321go@gmail.com>
2018-10-18 16:55:27 +0900

    delete host model and hosts table

--- app/models/host.rb
+++
@@ -1,7 +0,0 @@
-class Host < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :spaces, dependent: :destroy
-end
