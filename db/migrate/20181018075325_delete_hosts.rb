66c9c01aca1769a4509ed75249811bff08b82c60
manami-kk <manami.321go@gmail.com>
2018-10-18 16:55:27 +0900

    delete host model and hosts table

---
+++ db/migrate/20181018075325_delete_hosts.rb
@@ -0,0 +1,5 @@
+class DeleteHosts < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :hosts
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018040116) do
+ActiveRecord::Schema.define(version: 20181018075325) do
 
   create_table "basic_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "capacity",                null: false
@@ -49,19 +49,6 @@ ActiveRecord::Schema.define(version: 20181018040116) do
     t.datetime "updated_at",                   null: false
     t.integer  "space_id"
     t.index ["space_id"], name: "index_equipment_infos_on_space_id", using: :btree
-  end
-
-  create_table "hosts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "first_name",             null: false
-    t.string   "last_name",              null: false
-    t.string   "email",                  null: false
-    t.integer  "phone_number",           null: false
-    t.string   "encrypted_password",     null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
   end
 
   create_table "image_files", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -208,10 +195,10 @@ ActiveRecord::Schema.define(version: 20181018040116) do
 
   create_table "spaces", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "likes_count", default: 0
-    t.integer  "host_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.index ["host_id"], name: "index_spaces_on_host_id", using: :btree
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_spaces_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
