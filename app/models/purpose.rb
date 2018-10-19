38a26435e6d9418994e81d5d7f074c6657111f98
manami-kk <manami.321go@gmail.com>
2018-10-19 21:17:41 +0900

    edit validation for purpose model

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,15 +1,5 @@
 class Purpose < ApplicationRecord
-  belongs_to :basic_info, optional: true
+  belongs_to :basic_info
 
-  validates :party, presence: true
-  validates :meeting, presence: true
-  validates :photo_shoot, presence: true
-  validates :film_shoot, presence: true
-  validates :event, presence: true
-  validates :performance, presence: true
-  validates :studio, presence: true   
-  validates :sports, presence: true
-  validates :office, presence: true
-  validates :wedding, presence: true
-  validates :other, presence: true
+  validates_with PurposeValidationValidator
 end
--- app/validators/purpose_validation_validator.rb
+++ app/validators/purpose_validation_validator.rb
@@ -1,7 +1,10 @@
-class PurposeValidationValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if value.blank?
-      record.errors.add(attribute, '使用可能用途を入力してください。')
+class PurposeValidationValidator < ActiveModel::Validator
+  def validate(record)
+    items = [record.party, record.meeting, record.photo_shoot, record.film_shoot, record.event,
+      record.performance, record.studio, record.sports, record.office, record.wedding, record.other]
+    
+    unless items.any? {|item| item == 1}
+      record.errors[:base] << "使用可能用途を入力してください。"
     end
   end
 end
