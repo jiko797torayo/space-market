4558944fe7f45ea1b20b83c21f9faecbefde67a5
manami-kk <manami.321go@gmail.com>
2018-10-19 21:17:28 +0900

    edit validation for basic_infos model

--- app/validators/capacity_validation_validator.rb
+++ app/validators/capacity_validation_validator.rb
@@ -2,7 +2,8 @@ class CapacityValidationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.blank?
       record.errors.add(attribute, '最大収容人数を入力してください。')
-    elsif value !~ /^[1-9999]+$/
+    end
+    unless value.to_i > 0
       record.errors.add(attribute, '最大収容人数は不正な値です。')
     end
   end
