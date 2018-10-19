9cdccbe38e4839ae734f5c1a62debad491d6852a
manami-kk <manami.321go@gmail.com>
2018-10-19 21:17:28 +0900

    edit validation for basic_infos model

--- app/validators/floor_space_validation_validator.rb
+++ app/validators/floor_space_validation_validator.rb
@@ -2,7 +2,8 @@ class FloorSpaceValidationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.blank?
       record.errors.add(attribute, '床面積を入力してください。')
-    elsif value !~ /^[1-99999]+$/
+    end
+    unless value.to_i > 0
       record.errors.add(attribute, '床面積は不正な値です。')
     end
   end
