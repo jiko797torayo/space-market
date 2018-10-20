a0d68b6ae1f96fa65eeae8dc5c6a020cfe8ba742
manami-kk <manami.321go@gmail.com>
2018-10-20 19:16:45 +0900

    fixed validation for plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,9 +6,9 @@ class Plan < ApplicationRecord
   accepts_nested_attributes_for :rental_day
   accepts_nested_attributes_for :rental_hour
 
-  validates :name, presence: true
-  validates :price_per_day, presence: true
-  validates :reservation_approval_method, presence: true
+  validates :name, plan_name_validation: true
+  validates :price_per_day, price_validation: true
+  validates :reservation_approval_method, reservation_approval_method_validation: true
 
   enum reservation_approval_method: {
     承認なし（今すぐ予約）:1, 承認あり:2
