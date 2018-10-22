4d17b963ad44bc0ecb72cfe9ac6371aabbe4a0f1
manami-kk <manami.321go@gmail.com>
2018-10-22 23:36:14 +0900

    edit spaces controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,5 +4,10 @@ class SpacesController < ApplicationController
 
   def edit
     @space = Space.find(params[:id])
+    if @space.user_id == current_user.id
+      render layout: 'new_space'
+    else
+      redirect_to root_path
+    end
   end
 end
