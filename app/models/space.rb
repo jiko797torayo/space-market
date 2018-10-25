030e4f1bd9984a95dddf51769fe844ede3274ec3
manami-kk <manami.321go@gmail.com>
2018-10-25 12:36:01 +0900

    create methods to search spaces by purpose

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,4 +7,24 @@ class Space < ApplicationRecord
   has_one  :space_image, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :reservations, dependent: :destroy
+
+  def self.search_party_spaces
+    joins(basic_info: [:purpose]).where('party = ?', 1).order('likes_count DESC')
+  end
+
+  def self.search_meeting_spaces
+    joins(basic_info: [:purpose]).where('meeting = ?', 1).order('likes_count DESC')
+  end
+
+  def self.search_photo_shoot_spaces
+    joins(basic_info: [:purpose]).where('photo_shoot = ?', 1).order('likes_count DESC')
+  end
+
+  def self.search_film_shoot_spaces
+    joins(basic_info: [:purpose]).where('film_shoot = ?', 1).order('likes_count DESC') 
+  end
+
+  def self.search_sports_spaces
+    joins(basic_info: [:purpose]).where('sports = ?', 1).order('likes_count DESC')
+  end
 end
