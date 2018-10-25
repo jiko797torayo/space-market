aaa3a625d2628fd2ef99d5cdd461555f605aa362
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-25 14:41:50 +0900

    add search function by purpose button

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,10 @@ class SearchController < ApplicationController
   end
 
   def search
-    @spaces = Space.all.page(params[:page]).per(2)
+    @spaces = Space.all.page(params[:page]).per(20)
+    if params[:purpose]
+      @spaces = @spaces.joins(basic_info: [:purpose]).where("#{params[:purpose]} = ?", 1).page(params[:page]).per(20)
+    end
     pref = EquipmentInfo.prefectures.select{|n| n == params[:prefecture_key]}.values[0]
     appr = Plan.reservation_approval_methods.select{|n| n == params[:approval_method_key]}.values[0]
     unless params[:prefecture_key].blank?
@@ -22,5 +25,12 @@ class SearchController < ApplicationController
     unless params[:approval_method_key].blank?
       @spaces = @spaces.joins(:plan).where('reservation_approval_method = ?', "#{ appr }").page(params[:page]).per(20)
     end
+    purposes = %w(party meeting photo_shoot film_shoot event performance studio sports office wedding other)
+    purposes.each do |purpose|
+      if request.referer.include?("purpose=#{purpose}")
+        @spaces = @spaces.joins(basic_info: [:purpose]).where("#{purpose} = ?", 1).page(params[:page]).per(20)
+      end
+    end
+
   end
 end
