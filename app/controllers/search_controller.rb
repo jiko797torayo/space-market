3271512842ddc2a57c0f6d576ad004769530d5a5
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-25 10:07:06 +0900

    introduction pagenation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,23 +4,23 @@ class SearchController < ApplicationController
   end
 
   def search
-    @spaces = Space.all
+    @spaces = Space.all.page(params[:page]).per(2)
     pref = EquipmentInfo.prefectures.select{|n| n == params[:prefecture_key]}.values[0]
     appr = Plan.reservation_approval_methods.select{|n| n == params[:approval_method_key]}.values[0]
     unless params[:prefecture_key].blank?
-      @spaces = @spaces.joins(:equipment_info).where('prefecture LIKE(?)', "#{pref}")
+      @spaces = @spaces.joins(:equipment_info).where('prefecture LIKE(?)', "#{ pref }").page(params[:page]).per(20)
     end
     unless params[:price_min_key].blank?
-      @spaces = @spaces.joins(:plan).where('price_per_day > ?', "#{ params[:price_min_key] }")
+      @spaces = @spaces.joins(:plan).where('price_per_day > ?', "#{ params[:price_min_key] }").page(params[:page]).per(20)
     end
     unless params[:price_max_key].blank?
-      @spaces = @spaces.joins(:plan).where('price_per_day < ?', "#{ params[:price_max_key] }")
+      @spaces = @spaces.joins(:plan).where('price_per_day < ?', "#{ params[:price_max_key] }").page(params[:page]).per(20)
     end
     unless params[:capacity_key].blank?
-      @spaces = @spaces.joins(:basic_info).where('capacity > ?', "#{ params[:capacity_key] }")
+      @spaces = @spaces.joins(:basic_info).where('capacity > ?', "#{ params[:capacity_key] }").page(params[:page]).per(20)
     end
     unless params[:approval_method_key].blank?
-      @spaces = @spaces.joins(:plan).where('reservation_approval_method = ?', "#{ appr }")
+      @spaces = @spaces.joins(:plan).where('reservation_approval_method = ?', "#{ appr }").page(params[:page]).per(20)
     end
   end
 end
