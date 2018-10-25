27e038670cd481cb0509732c7534c0271569a9bf
井川嵯介 <sasuke7779@gmail.com>
2018-10-25 18:14:14 +0900

    Edit reservation controller&models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,9 @@ class Reservation < ApplicationRecord
   belongs_to :user
   has_one :reservation_hour, dependent: :destroy
   belongs_to :space
+
+  validates :user_id,    presence: true
+  validates :space_id,   presence: true
+  validates :day,        presence: true
 end
+
