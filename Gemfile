08597e135a0885f227216a4f1fe10d55edd20d83
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-25 17:21:14 +0900

    add purpose block highlight

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'devise'
 gem 'kaminari'
+gem 'gon'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/assets/javascripts/purpose-highlight.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+  var purposeClass = `.search--header--type__nav__${ gon.purpose_key }`;
+  var purpose = gon.purpose_key;
+  console.log(purpose);
+  $(purposeClass).addClass('nav-highlight');
+
+  var others = { 'film_shoot': 'ロケ撮影' , 'studio': '展示会', 'performance': '演奏', 'sports': 'スポーツ', 'office': 'オフィス', 'wedding': '結婚式', 'event': 'イベント', 'other': 'その他' };
+  for(var key in others) {
+    if(key == purpose) {
+      console.log(key);
+      $('.nav-others__title').text(others[key]).addClass('nav-highlight');
+    }
+  }
+});
