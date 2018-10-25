418c4df5b9217deb867c9b49759733074925c88d
manami-kk <manami.321go@gmail.com>
2018-10-25 12:36:01 +0900

    create methods to search spaces by purpose

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,6 +2,13 @@ class SpacesController < ApplicationController
   before_action :check_current_user, only: :edit
   
   def index
+    @party_spaces = Space.search_party_spaces.limit(3)
+    @meeting_spaces = Space.search_meeting_spaces.limit(3)
+    @photo_shoot_spaces = Space.search_photo_shoot_spaces.limit(3)
+    @film_shoot_spaces = Space.search_film_shoot_spaces.limit(3)
+    @sports_spaces = Space.search_sports_spaces.limit(3)
+    #いいね機能を実装後に修正する
+    @favorite_spaces = Space.all.limit(3)
   end
 
   def edit
