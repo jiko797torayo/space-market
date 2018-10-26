24865b97a8a15a95b3c8bbf449f97ab6b17052cb
manami-kk <manami.321go@gmail.com>
2018-10-26 14:38:46 +0900

    add scope published

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -5,13 +5,13 @@ class SpacesController < ApplicationController
   before_action :set_space, only: [:edit, :update]
   
   def index
-    @party_spaces = Space.party.by_likes_count.limit(3)
-    @meeting_spaces = Space.meeting.by_likes_count.limit(3)
-    @photo_shoot_spaces = Space.photo_shoot.by_likes_count.limit(3)
-    @film_shoot_spaces = Space.film_shoot.by_likes_count.limit(3)
-    @sports_spaces = Space.sports.by_likes_count.limit(3)
+    @party_spaces = Space.published.party.by_likes_count.limit(3)
+    @meeting_spaces = Space.published.meeting.by_likes_count.limit(3)
+    @photo_shoot_spaces = Space.published.photo_shoot.by_likes_count.limit(3)
+    @film_shoot_spaces = Space.published.film_shoot.by_likes_count.limit(3)
+    @sports_spaces = Space.published.sports.by_likes_count.limit(3)
     #いいね機能を実装後に修正する
-    @favorite_spaces = Space.all.limit(3)
+    @favorite_spaces = Space.published.all.limit(3)
   end
 
   def edit
