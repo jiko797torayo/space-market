93ebf61574fad751fb201c58728ed36ead0b7bad
井川嵯介 <sasuke7779@gmail.com>
2018-10-26 11:57:37 +0900

    Fix validates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,8 +3,6 @@ class Reservation < ApplicationRecord
   has_one :reservation_hour, dependent: :destroy
   belongs_to :space
 
-  validates :user_id,    presence: true
-  validates :space_id,   presence: true
   validates :day,        presence: true
 end
 
