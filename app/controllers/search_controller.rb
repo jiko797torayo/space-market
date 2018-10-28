5d8e8983c95be2ba464349cb5c095f321ab8ed2b
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-28 18:27:27 +0900

    get the purpose key from the url

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,21 +4,10 @@ class SearchController < ApplicationController
   end
 
   def search
-    @spaces = Space.prefecture_key(params[:prefecture_key]).price_min_key(params[:price_min_key]).price_max_key(params[:price_max_key]).capacity_key(params[:capacity_key]).approval_method_key(params[:approval_method_key]).page(params[:page]).per(20)
+    @spaces = Space.prefecture_key(params[:prefecture_key]).price_min_key(params[:price_min_key]).price_max_key(params[:price_max_key]).capacity_key(params[:capacity_key]).approval_method_key(params[:approval_method_key]).purpose_key(request.url.scan(/purpose_key=([a-z_]+)/).join).page(params[:page]).per(20)
     parameter_keys = [:prefecture_key, :price_min_key, :price_max_key, :capacity_key, :approval_method_key, :purpose_key]
     parameter_keys.each do |parameter_key|
-      unless params[parameter_key[0]].blank?
-        @spaces = @spaces.joins(parameter_key[1]).where(parameter_key[2], parameter_key[3])
-        instance_variable_set("@exit_#{ parameter_key[0].to_s }", params[parameter_key[0]])
-      end
     end
-
-    Purpose.column_names.each do |purpose|
-      if request.url.include?("purpose_key=#{purpose}")
-        @spaces = @spaces.joins(basic_info: [:purpose]).where("#{purpose} = ?", 1)
-        @exit_purpose_key = purpose
-        gon.purpose_key = purpose
-      end
-    end
+    gon.purpose_key = request.url.scan(/purpose_key=([a-z_]+)/).join
   end
 end
