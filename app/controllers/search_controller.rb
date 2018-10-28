060773e3a2c2464123b63322b5fec0312af5e637
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-10-28 18:29:33 +0900

    fix taking over the search condition function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,7 @@ class SearchController < ApplicationController
     @spaces = Space.prefecture_key(params[:prefecture_key]).price_min_key(params[:price_min_key]).price_max_key(params[:price_max_key]).capacity_key(params[:capacity_key]).approval_method_key(params[:approval_method_key]).purpose_key(request.url.scan(/purpose_key=([a-z_]+)/).join).page(params[:page]).per(20)
     parameter_keys = [:prefecture_key, :price_min_key, :price_max_key, :capacity_key, :approval_method_key, :purpose_key]
     parameter_keys.each do |parameter_key|
+      instance_variable_set("@exit_#{ parameter_key.to_s }", params[parameter_key]) if params[parameter_key].present?
     end
     gon.purpose_key = request.url.scan(/purpose_key=([a-z_]+)/).join
   end
