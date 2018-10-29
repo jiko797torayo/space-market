a27bc6ce3354dc9cf79b1478b85ddb780f04280d
manami-kk <manami.321go@gmail.com>
2018-10-29 15:54:16 +0900

    fixed controllers to set space id

--- app/controllers/space_images_controller.rb
+++ app/controllers/space_images_controller.rb
@@ -6,19 +6,19 @@ class SpaceImagesController < ApplicationController
   before_action :set_space_image, only: [:edit, :update]
 
   def new
-    @@space = Space.find_by(id: params[:space_id])
+    @space_id = params[:space_id]
     @space_image = SpaceImage.new
     @space_image.image_files.build
   end
 
   def create
     @space_image = SpaceImage.new(space_image_params)
-    @space_image.space = @@space
 
     # 0番目（メイン）のfileカラムにデータが無い場合のバリデーション
     upload_file = space_image_params[:image_files_attributes]["0"][:file]
     if upload_file == nil
-      redirect_to new_space_image_path(space_id: @@space.id), alert: '画像を最低1枚選択してください' and return
+      set_space_id
+      redirect_to new_space_image_path(space_id: @space_id), alert: '画像を最低1枚選択してください' and return
     end
 
     @space_image.save
@@ -26,16 +26,15 @@ class SpaceImagesController < ApplicationController
   end
 
   def edit
-    set_space_image
     @main_image = @space_image.image_files.main.first
     @sub_images = @space_image.image_files.sub
   end
 
   def update
-    set_space_image
     if @space_image.update(space_image_params)
-      redirect_to edit_space_path(@space_image.space)
+      redirect_to edit_space_path(@space_image.space_id)
     else
+      set_space_id
       render :edit
     end
   end
@@ -52,11 +51,15 @@ class SpaceImagesController < ApplicationController
   end
 
   def space_image_params
-    params.require(:space_image).permit(image_files_attributes: [:id, :file, :about_file, :status])
+    params.require(:space_image).permit(:space_id, image_files_attributes: [:id, :file, :about_file, :status])
+  end
+
+  def set_space_id
+    @space_id = params[:space_image][:space_id]
   end
 
   def next_page
-    redirect_to edit_space_path(@@space) if params[:commit] == "保存して戻る"
-    redirect_to new_plan_path(space_id: @@space.id) if params[:commit] == "保存して進む"
+    redirect_to edit_space_path(@space_image.space_id) if params[:commit] == "保存して戻る"
+    redirect_to new_plan_path(space_id: @space_image.space_id) if params[:commit] == "保存して進む"
   end
 end
