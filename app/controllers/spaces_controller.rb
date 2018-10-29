51092253c820a60be3bbb924740382d95c714e4d
井川嵯介 <sasuke7779@gmail.com>
2018-10-29 15:02:34 +0900

    Make detail view
    スペース詳細画面のビュー

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,9 @@
 class SpacesController < ApplicationController
   def index
   end
+  def show
+    @space = Space.find(params[:id])
+    @sub_images = @space.space_image.image_files.sub
+  end
+  private
 end
