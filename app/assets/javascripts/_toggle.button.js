81b17527b50ca85a5017d8998cdcd000b190f8be
井川嵯介 <sasuke7779@gmail.com>
2018-10-31 17:51:27 +0900

    マイページの修正

--- app/assets/javascripts/_toggle.button.js
+++ app/assets/javascripts/_toggle.button.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function() {
     // 予約一覧とあなたのスペース一覧の切り替え
   $(function() {
     $('.toggle-button').on('click', function() {
-      $('.reservation-list').toggle();
+      $('.user-space').toggle();
       $('.none').toggle();
       $('.reserve').toggleClass('check')
       $('.userSpace').toggleClass('check')
