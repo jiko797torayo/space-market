23329aef85506ce905ebecb8b2673e706ef5b74c
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-11-01 21:18:03 +0900

    fix mypage view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @reservations = Reservation.where(user_id: params[:id]).order('day ASC')
+    @reservations = Reservation.where(user_id: params[:id])
     @spaces = Space.where(user_id: params[:id])
   end
 end
