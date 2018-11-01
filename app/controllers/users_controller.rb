d3f6b6228af2b480c83cecc7e28bca3e3d212880
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-11-01 21:40:37 +0900

    enhanced security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-
+  before_action :check_current_user, only: [:show]
   def show
-    @reservations = Reservation.where(user_id: params[:id]).order('day ASC')
+    @reservations = Reservation.where(user_id: params[:id])
     @spaces = Space.where(user_id: params[:id])
+  end
+
+  private
+  def check_current_user
+    render_404 unless user_signed_in? && (current_user.id == params[:id].to_i)
   end
 end
